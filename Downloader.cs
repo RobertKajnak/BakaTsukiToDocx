b68fd03d944cc831cf4b8953c68a4f889c56de0f
robertkajnak <hezirisz@gmail.com>
2017-03-12 11:32:30 +0200

    Changed per-character comparison to per-tag

--- Downloader.cs
+++ Downloader.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Baka_Tsuki_Downloader
 {
@@ -30,77 +31,7 @@ namespace Baka_Tsuki_Downloader
             System.IO.File.WriteAllText(fileName, html);
 
         }
-
-        /*public static void ConvertOld(string sourcefile, string destFile)
-        {
-            Logger.Start("Read & Convert");
-            string html = File.ReadAllText(path + sourcefile);
-            Console.WriteLine("File read");
-            WordWriter wordWriter = new WordWriter(destFile);
-            Console.WriteLine("WordBuilder created");
-
-            StringParser content = new StringParser(html);
-
-
-            StringParser titleHtml = new StringParser(content.Substring("<title>", "</title"));
-            string title = titleHtml.SubstringLast(null, ":");
-            int volume;
-            Int32.TryParse(titleHtml.SubstringLast(":Volume", " - Baka-Tsuki").ToString(), out volume);
-
-            wordWriter.Title(title, "Yuu", volume);
-
-
-            //TOOD:Handle <Pre>, <li>, images, furigana substitute
-            content = new StringParser(content.Substring("<h2>", "<td> Back to <a"));
-
-            //StringParser Chapter = new StringParser(content.Substring(null, "<h2>"));
-            //Chapter = new StringParser(Chapter.Substring("<p>", null));
-            string[] chapters = content.ToString().Split(chapterSeparator, StringSplitOptions.RemoveEmptyEntries);
-
-            Logger.Stop("Read & Convert");
-            Logger.Start("Write");
-            int i = 0;
-            foreach (string chapter in chapters)
-            {
-                if (i >= 3)
-                    break;
-                if (i == 0)
-                {
-                    i++;
-                    continue;
-                }
-                wordWriter.Chapter("Prologue");
-
-                //inserting everything at once with \n instead of paragraph by paragraph increased speed by ~1000 times
-                //string c = chapter.Replace("<p>", "").Replace("</p>","").Replace("<br />","\n");
-                // wordWriter.Paragraph(c);
-
-                List<string> chapterlist = chapter.Split(lineSeparators, StringSplitOptions.RemoveEmptyEntries).ToList();
-                chapterlist.ForEach(s => s.Split(chapterSeparator, StringSplitOptions.RemoveEmptyEntries));
-
-
-                foreach (string line in chapterlist)
-                {
-                    if (line.Contains("<br />") || line.Contains("<br/>"))
-                    {
-                        string l = line.Replace("<br />", "");
-                        wordWriter.Paragraph(l);
-                        wordWriter.Paragraph("");//need to adjust space after instead
-                    }
-                    else
-                    {
-                        wordWriter.Paragraph(line);
-                    }
-                }
-                Console.WriteLine("Chapter " + i++ + "finished");
-                Logger.Lap("Write");
-
-            }
-            Logger.Stop("Write");
-            Logger.PutDelimiter();
-            Console.WriteLine("Writing data to Word document finished");
-            wordWriter.SaveAndQuit();
-        }*/
+        
 
         public static void Convert(string sourcefile, string destFile)
         {
@@ -160,37 +91,35 @@ namespace Baka_Tsuki_Downloader
                 {
                     int firstIndex = chapterContent.IndexOf('<');
 
+                    ///case 1: there is a tag, and it is at the beginning of the text
                     if (firstIndex == 0)
                     {
-                        switch (chapterContent[1])
+                        int closeBracketIndex = chapterContent.IndexOf('>');
+                        closeBracketIndex = closeBracketIndex != -1 ? closeBracketIndex : chapterContent.Length;
+                        string tag = chapterContent.Substring(0, closeBracketIndex + 1);
+
+                        switch (TagType.getType(tag))
                         {
-                            case 'h':
-                                switch (chapterContent[2])
-                                {
-                                    case '2':
-                                        Console.WriteLine("chapter title has not been handled properly");
-                                        break;
-                                    default:
-                                        Console.WriteLine("Unrecognized header: " + chapterContent[2]);
-                                        ///TODO this only handles single digit headers
-                                        chapterContent = chapterContent.Substring(chapterContent.IndexOf("</h" + chapterContent[2] + ">") + 5);
-                                        break;
-                                }
+                            case TagType.Type.h1:
+                                break;
+                            case TagType.Type.h2:
+                                Console.WriteLine("chapter title has not been handled properly");
                                 break;
                             default:
-                                int closeBracketIndex = chapterContent.IndexOf('>');
-                                closeBracketIndex = closeBracketIndex != -1 ? closeBracketIndex : chapterContent.Length;
-                                Console.WriteLine("Uninterpreted tag: " + chapterContent.Substring(0, closeBracketIndex + 1));
+                                Console.WriteLine("Uninterpreted tag: " + tag);
                                 chapterContent = chapterContent.Substring(closeBracketIndex + 1);
                                 break;
                         }
+                        
                         firstIndex = chapterContent.IndexOf('<');
                     }
+                    ///case 2: there is a tag, but it is not at the beginning, so the text up to that point is copied
                     else if (firstIndex != -1 && chapterContent.Length != 0)
                     {
                         wordWriter.Paragraph(chapterContent.Substring(0, firstIndex));
                         chapterContent = chapterContent.Substring(firstIndex);
                     }
+                    ///case 3 there are no tags, but the text is non-zero length, i.e. it is a tag-free text
                     else if  (firstIndex == -1 && chapterContent.Length != 0)
                     {
                         wordWriter.Paragraph(chapterContent);
---
+++ TagType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Baka_Tsuki_Downloader
+{
+    static class TagType
+    {
+        public enum Type {h1, h2, h3, uncategorized};
+        private static string h1 = "<h1>", h2 = "<h2>";
+        public static Type getType(string tag)
+        {
+            if (Regex.IsMatch(tag, h1))
+                return Type.h1;
+            else if (tag.Equals(h2))
+                return Type.h2;
+            else
+            return Type.uncategorized;
+        }
+    }
+}
